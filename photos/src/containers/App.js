cb4686c02cd9aa5d4f42a53e8f2330b0019d7f95
Dmitry Gorkovenko <Dmitry.Gorkovenko@infotecs.ru>
2016-06-29 11:54:11 +0300

    комбинирование редьюсеров

--- photos/src/containers/App.js
+++ photos/src/containers/App.js
@@ -3,17 +3,19 @@ import { connect } from 'react-redux'
 
 export default class App extends Component {
     render() {
-        const { name, surname, age} = this.props.user
+        const { name, surname } = this.props.user
+        const { photos, year} = this.props.page
         return <div>
-            <p>Привет из App, { name } { surname }</p>
-            <p>{ age }</p>
+            <p>Привет, { name } { surname }!</p>
+            <p>У тебя { photos.length } фото за { year }</p>
         </div>
     }
 }
 
 function mapStateToProps (state) {
     return {
-        user: state
+        user: state.user,
+        page: state.page
     }
 }
 
--- photos/src/reducers/index.js
+++ photos/src/reducers/index.js
@@ -1,9 +1,8 @@
-const initialState = {
-    name: 'Name',
-    surname: 'Surname',
-    age: 20
-}
+import { combineReducers } from 'redux'
+import page from './page'
+import user from './user'
 
-export default function userstate(state = initialState) {
-    return state
-}
+export default combineReducers({
+    page,
+    user
+})
