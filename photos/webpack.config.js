48b0f21b88fb58a171521299e5961608631243b4
Dmitry Gorkovenko <Dmitry.Gorkovenko@infotecs.ru>
2016-07-04 16:23:24 +0300

    добавил файлы стилей

--- photos/webpack.config.js
+++ photos/webpack.config.js
@@ -1,5 +1,8 @@
 var path = require('path')
 var webpack = require('webpack')
+var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var precss = require('precss')
+var autoprefixer = require('autoprefixer')
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
@@ -16,7 +19,8 @@ module.exports = {
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new NpmInstallPlugin()
     ],
     module: {
         preLoaders: [
@@ -36,7 +40,14 @@ module.exports = {
                 ],
                 test: /\.js$/,
                 plugins: ['transform-runtime']
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!postcss-loader"
             }
         ]
+    },
+    postcss: function () {
+        return [autoprefixer, precss]
     }
 }
